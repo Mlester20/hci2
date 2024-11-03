35cbb9afd4c4b9d3de6e2a49bdac341976250335
Mark Lester Raguindin <104201271+Mlester20@users.noreply.github.com>
2024-11-03 18:45:25 +0800

    Update teacher_save.php

--- pages/teacher_save.php
+++ pages/teacher_save.php
@@ -1,38 +1,36 @@
-
- <?php session_start();
+<?php 
+session_start();
 if(empty($_SESSION['id'])):
-header('Location:../index.php');
+    header('Location:../index.php');
+    exit();
 endif;
 
 include('../dist/includes/dbcon.php');
 
-	$salut = $_POST['salut'];	
-	$last = $_POST['last'];	
-	$first = $_POST['first'];	
-	$rank = $_POST['rank'];	
-	$dept = $_POST['dept'];	
-	$designation = $_POST['designation'];	
-	$username =preg_replace('/\s+/','',$first).$dept;	
-	$username=strtolower($username);
-	$password = preg_replace('/\s+/','',$last);	
-	$password=strtolower($password);	
-	$status = $_POST['status'];	
-					
-		$query=mysqli_query($con,"select * from member where member_last='$last' and  member_first='$first'")or die(mysqli_error());
-				$count=mysqli_num_rows($query);
-				if ($count>0)
-				{
-					echo "<script type='text/javascript'>alert('Member already exist');</script>";
-					echo "<script>document.location='teacher.php'</script>";  
-				}	
-				else{
-					mysqli_query($con,"INSERT INTO member(member_salut,member_last,member_first,member_rank,dept_code,designation_id,username,password,status) 
-					VALUES('$salut','$last','$first','$rank','$dept','$designation','$username','$password','$status')")or die(mysqli_error());
-				
-					echo "<script type='text/javascript'>
-				alert('Successfuly added new member');</script>";	
-				echo "<script>document.location='teacher.php'</script>";  
-				}
-				  
-	
+$salut = $_POST['salut'];    
+$last = $_POST['last'];    
+$first = $_POST['first'];    
+$rank = $_POST['rank'];    
+$dept = $_POST['dept'];    
+$designation = $_POST['designation'];    
+$username = preg_replace('/\s+/','', $first);  // Remove dept from username
+$username = strtolower($username);
+$password = preg_replace('/\s+/','', $last);
+$password = strtolower($password);
+$hashed_password = password_hash($password, PASSWORD_DEFAULT);  // Encrypt the password
+$status = $_POST['status'];    
+                
+$query = mysqli_query($con, "SELECT * FROM member WHERE member_last='$last' AND member_first='$first'") or die(mysqli_error($con));
+$count = mysqli_num_rows($query);
+
+if ($count > 0) {
+    echo "<script type='text/javascript'>alert('Member already exists');</script>";
+    echo "<script>document.location='teacher.php'</script>";  
+} else {
+    mysqli_query($con, "INSERT INTO member (member_salut, member_last, member_first, member_rank, dept_code, designation_id, username, password, status) 
+    VALUES ('$salut', '$last', '$first', '$rank', '$dept', '$designation', '$username', '$hashed_password', '$status')") or die(mysqli_error($con));
+    
+    echo "<script type='text/javascript'>alert('Successfully added new member');</script>";    
+    echo "<script>document.location='teacher.php'</script>";  
+}
 ?>
