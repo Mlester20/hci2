d16e84a2af7564256b3c006e8faf90f287b23dcb
Mark Lester Raguindin <104201271+Mlester20@users.noreply.github.com>
2024-11-14 17:57:53 +0800

    Update logo.php

--- dist/includes/logo.php
+++ dist/includes/logo.php
@@ -1,3 +1,11 @@
 <?php
-   
+  if($_SESSION() 
 ?>
+
+<img src="../images/isu-logo.png" width="60px" height="60px" class="logo">
+<img src="../images/isu-logo.png" width="60px" height="60px" class="logo2" style="display:hidden">
+        <h5 align="center">
+            Isabela State University</br>
+            Roxas, Isabela</br><br><br>
+            CLASS <?php echo strtoupper($rows['term']); ?> EXAM SCHEDULE</br>
+        </h5>
